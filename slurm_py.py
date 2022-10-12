a30fdd58c8e70aec4c57080b323ffe70e6157395
Gabriel Serrao Seabra <g.serraoseabra@tudelft.nl>
2022-10-12 16:01:30 +0200

    INITIAL COMMIT

---
+++ slurm_py.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+
+def mkdir_p(dir):
+   '''make a directory (dir) if it doesn't exist'''
+   if not os.path.exists(dir):
+       os.mkdir(dir)
+    
+
+job_directory = os.getcwd()
+
+data_dir = os.path.join(job_directory, 'project')
+
+# Make top level directories
+mkdir_p(job_directory)
+mkdir_p(data_dir)
+
+lizards=["LizardA","LizardB"]
+
+for lizard in lizards:
+
+    job_file = os.path.join(job_directory,"%s.job" %lizard)
+    with open(job_file, 'w+') as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH --job-name=%s\n" % lizard)
+        fh.writelines("#SBATCH --output=%s\n" % lizard)
+        fh.writelines("#SBATCH --error=%s\n" % lizard)
+        fh.writelines("#SBATCH --time=2:00:00\n")
+        fh.writelines("#SBATCH --mem=1G\n")
+        fh.writelines("#SBATCH --partition=compute\n")
+        fh.writelines("#SBATCH --account=research-ceg-gse\n")
+        fh.writelines(f'a={str(lizard)}\n')
+        ##fh.writelines(f'b={str(destDir)}\n')
+        ##fh.writelines(f'c={str(time_range[-1])}\n')
+        fh.writelines(f'python teste_gpu.py $a\n')
+        ##fh.writelines("python teste_gpu.py $SLURM_SUBMIT_DIR\n")
+
+    os.system("sbatch %s" %job_file)
---
+++ teste_gpu.py
@@ -0,0 +1,18 @@
+#import tensorflow
+#create a randon array, plot and save figure
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import darts
+print(f'primeiro argumento {sys.argv[1]}')
+
+#print(f'segundo argumento {sys.argv[2]}')
+
+#print(f'segundo argumento {sys.argv[3]}')
+
+
+# #print(sys.argv[2])
+# x = np.random.rand(100,100)
+# plt.imshow(x)
+# plt.savefig(f'teste{task_id}.png')
